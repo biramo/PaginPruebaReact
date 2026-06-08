50ed37a0547b41e51f160a8ef145d0652c86c0bd
biramo <arnau.roy.ramos@gmail.com>
2026-06-08 20:35:21 +0200

    .gitIgnore actualizado

--- src/Componentes/Loggin/Login.jsx
+++ src/Componentes/Loggin/Login.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from 'react-router-dom'  
+import { sendPasswordResetEmail } from "firebase/auth"; 
+import { auth } from "../DB_firebase/firebase";
+
 export default function Login() {
   const { user, login, register, logout } = useAuth();
   const [email, setEmail] = useState("");
@@ -8,6 +11,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isRegistering, setIsRegistering] = useState(false);
+  const [mensajeReset, setMensajeReset] = useState(""); 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,9 +29,26 @@ export default function Login() {
     }
   };
 
+  //Maneja el cambio de contraseña del usuario
+  const handleResetPassword = async () => {
+    if (!email) {
+      setError("Escribe tu email primero para recuperar la contraseña");
+      return;
+    }
+    try {
+      await sendPasswordResetEmail(auth, email);
+          console.log("Email enviado a:", email);  // ← añade esto
+      setMensajeReset("Te hemos enviado un email para restablecer tu contraseña");
+      setError("");
+    } catch (err) {
+      setError("No se encontró ninguna cuenta con ese email");
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit} class="form-inicio-sesion">
+    <form onSubmit={handleSubmit} className="form-inicio-sesion">
       <h2>{isRegistering ? "Crear cuenta" : "Iniciar sesión"}</h2>
+      {mensajeReset && <p>{mensajeReset}</p>}
       {error && <p>{error}</p>}
       <input
         type="email"
@@ -46,6 +67,13 @@ export default function Login() {
       <button type="submit" className="init-sesion-btn">
         {isRegistering ? "Registrarse" : "Entrar"}
       </button>
+
+      {!isRegistering && (
+        <span className="auth-link" onClick={handleResetPassword}>
+          ¿Olvidaste tu contraseña?
+        </span>
+      )}
+
       <p onClick={() => {
         setIsRegistering(!isRegistering);
         setEmail("");
